932eb18a696143ac8b518628c2f2530dbaeefba7
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-05-06 04:23:51 +0300

    Solved problem

---
+++ capitalize_the_title.py
@@ -0,0 +1,17 @@
+#You are given a string title consisting of one or more words separated by a single space, where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
+
+#If the length of the word is 1 or 2 letters, change all letters to lowercase.
+#Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+#Return the capitalized title.
+
+
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        title = title.lower().split()
+        res = list()
+        for word in title:
+            if len(word) > 2:
+                res.append(word.capitalize())
+            else:
+                res.append(word)
+        return " ".join(res)
