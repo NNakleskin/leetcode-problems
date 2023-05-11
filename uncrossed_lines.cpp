fca2c85bc8393b339cdc8ace6c1fffe6f0745b5a
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-05-12 00:51:52 +0300

    Solved problem

---
+++ uncrossed_lines.cpp
@@ -0,0 +1,32 @@
+/*
+    You are given two integer arrays nums1 and nums2. We write the integers of nums1 and nums2 (in the order they are given) on two separate horizontal lines.
+
+We may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
+
+nums1[i] == nums2[j], and
+the line we draw does not intersect any other connecting (non-horizontal) line.
+Note that a connecting line cannot intersect even at the endpoints (i.e., each number can only belong to one connecting line).
+
+Return the maximum number of connecting lines we can draw in this way.
+*/
+
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> cur(n2 + 1), prev(n2 + 1);
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    cur[j] = 1 + prev[j - 1];
+                } else {
+                    cur[j] = max(cur[j - 1], prev[j]);
+                }
+            }
+            prev = cur;
+        }
+        return cur[n2];
+    }
+};
