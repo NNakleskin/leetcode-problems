633f01e009cd435c0f08893ac8ab11c015f1765c
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-07-04 13:58:58 +0300

    Solved problem

---
+++ smallest_even_multiple.cpp
@@ -0,0 +1,9 @@
+// Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        return n % 2 == 0 ? n : 2 * n;
+    }
+};
